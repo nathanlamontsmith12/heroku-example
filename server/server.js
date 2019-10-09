2f1905e869f84cf7b83026ccc1e0bab9c64f250b
Dave Silva <david.b.silva@accenture.com>
2019-10-09 13:18:47 -0400

    Separate frontend and server into subdirectories

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send(
-    `<h1>Welcome!</h1><p>The PORT is: ${port}</p><p>The DATABASE_URL is: ${
-      process.env.DATABASE_URL
-    }</p><p>See the <a target="_blank" href="https://github.com/mattlgroff/example-heroku-app/blob/master/README.md">Heroku Workshop Readme Here</a></p>`
-  );
+  res.send(`<h1>Welcome!</h1><p>The PORT is: ${port}</p>`);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
