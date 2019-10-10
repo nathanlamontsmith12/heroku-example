8e34cee5e1dae726fa94cb105c067e7c52e76213
nathanlamontsmith12 <andinto@nathanlamontsmith12.com>
2019-10-10 18:06:34 -0500

    basic setup w sequelize and postgres working

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,8 +1,41 @@
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+
+// NOTE: postgres connection string format is 
+// postgres:// <user> @localhost: <port> / <psql_database_name> 
+// without spaces 
+// find port in psql via \conninfo command 
 
 const DB_CONNECTION_STRING = process.env.DATABASE_URI;
-
-console.log(DB_CONNECTION_STRING);
 const sequelize = new Sequelize(DB_CONNECTION_STRING);
 
-module.exports = sequelize; 
+sequelize.authenticate()
+    .then( ()=> {
+        console.log("CONNECTED");
+    })
+    .catch( (err) => {
+        console.error("DID NOT CONNECT", err)
+    });
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    username: {type: DataTypes.STRING},
+    is_admin: {type: DataTypes.BOOLEAN}
+}, {
+    timestamps: false
+    // omitNull: true
+}); 
+
+// THIS IS A GOTCHA: if you don't pass the timestamps: false option, sequelize will automatically 
+// try to insert createdAt and updatedAt values into EVERY record created on EVERY model 
+// kind of annoying -- this should be opt-in, not opt-out, but w/e 
+
+// also, bc we have postgres handling ids as primary keys via SERIAL, 
+// we need to set omitNull to true -- otherwise sequelize will try to create 
+// the ids itself, with value NULL.... 
+
+// hmm..  couldn't get omitNull to work, but there's a workaround using fields in the create() call 
+
+
+module.exports = {
+    User
+}; 
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const express = require('express');
 const app = express();
 
 // sequelize 
-const sequelize = require('./db/sequelize.js');
+const { User } = require('./db/sequelize.js');
 
-console.log(sequelize);
+// console.log(sequelize);
 
 
 // Set port to default to the PORT in the environmental vars (like Heroku)
@@ -17,5 +17,26 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send(`<h1>Welcome!</h1><p>The PORT is: ${port} && ${process.env.TESTING}</p>`);
 });
+
+app.get('/nate', async (req, res) => {
+  try {
+
+    const nate = await User.create({
+      username: "Nathan Smith", 
+      is_admin: true
+    }, { fields: ["username", "is_admin"] }); 
+    
+    // the FIELDS object tells sequelize to only concern itself with those fields in creation; this way IDs are ignored 
+    // (bc we have set up postgres to make those automatically and we don't want sequelize to try to insert them)
+    
+    console.log(nate);
+
+    res.send(nate); 
+
+  } catch (err) {
+    console.log(err); 
+    res.send(err);
+  }
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
