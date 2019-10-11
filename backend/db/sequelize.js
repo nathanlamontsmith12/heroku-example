a7f1929626b1ce7373d2c307e555ab27ea47db2f
nathanlamontsmith12 <andinto@nathanlamontsmith12.com>
2019-10-11 10:09:16 -0500

    uses sequelize.sync({force: true}) to generate tables in example

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -8,34 +8,24 @@ const { Sequelize, DataTypes } = require('sequelize');
 const DB_CONNECTION_STRING = process.env.DATABASE_URI;
 const sequelize = new Sequelize(DB_CONNECTION_STRING);
 
-sequelize.authenticate()
-    .then( ()=> {
-        console.log("CONNECTED");
-    })
-    .catch( (err) => {
-        console.error("DID NOT CONNECT", err)
-    });
-
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true},
     username: {type: DataTypes.STRING},
     is_admin: {type: DataTypes.BOOLEAN}
-}, {
-    timestamps: false
-    // omitNull: true
 }); 
 
-// THIS IS A GOTCHA: if you don't pass the timestamps: false option, sequelize will automatically 
-// try to insert createdAt and updatedAt values into EVERY record created on EVERY model 
-// kind of annoying -- this should be opt-in, not opt-out, but w/e 
+const Category = sequelize.define('category', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    title: {type: DataTypes.STRING}
+})
 
-// also, bc we have postgres handling ids as primary keys via SERIAL, 
-// we need to set omitNull to true -- otherwise sequelize will try to create 
-// the ids itself, with value NULL.... 
 
-// hmm..  couldn't get omitNull to work, but there's a workaround using fields in the create() call 
+User.belongsToMany(Category, {through: "users_categories"});
+Category.belongsToMany(User, {through: "users_categories"});
 
+sequelize.sync({force: true});
 
 module.exports = {
-    User
+    User,
+    Category
 }; 
